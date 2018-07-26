16a054e79faa069927181355d884b4783dfcf7e2
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-25 18:41:36 -0700

    Test GET /styles.css

---
+++ test/styles.test.js
@@ -0,0 +1,23 @@
+var concat = require('simple-concat')
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+tape.test('GET /styles.css', function (test) {
+  server(function (port, done) {
+    http.request({path: '/styles.css', port: port})
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 200,
+          'responds 200'
+        )
+        test.assert(
+          response.headers['content-type'].includes('text/css'),
+          'text/css'
+        )
+        test.end()
+        done()
+      })
+      .end()
+  })
+})
