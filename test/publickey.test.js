967db50c058f96daded0715235bcdfc59a0edd59
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-25 18:42:56 -0700

    Test GET /publickey

---
+++ test/publickey.test.js
@@ -0,0 +1,27 @@
+var concat = require('simple-concat')
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+tape.test('GET /publickey', function (test) {
+  server(function (port, done) {
+    http.request({path: '/publickey', port})
+      .once('response', function (response) {
+        test.equal(
+          response.statusCode, 200,
+          'responds 200'
+        )
+        concat(response, function (error, buffer) {
+          test.ifError(error, 'no error')
+          var body = buffer.toString()
+          test.assert(
+            /^[a-f0-9]{64}$/.test(buffer.toString()),
+            'hex public key'
+          )
+          test.end()
+          done()
+        })
+      })
+      .end()
+  })
+})
