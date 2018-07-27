522b80ef839a524e638599099619d32f3048456a
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-26 18:09:36 -0700

    Refactor test to pull out add helper

---
+++ test/add.js
@@ -0,0 +1,53 @@
+var concat = require('simple-concat')
+var constants = require('./constants')
+var http = require('http')
+var mailgun = require('../mailgun/test').events
+var makeKeyPair = require('./make-key-pair')
+var sign = require('./sign')
+
+module.exports = function (email, port, test, callback) {
+  var keyPair = makeKeyPair()
+  var message = {
+    name: 'test device',
+    date: new Date().toISOString(),
+    email
+  }
+  var add = {
+    publicKey: keyPair.publicKey.toString('hex'),
+    signature: sign(message, keyPair.secretKey).toString('hex'),
+    message
+  }
+  mailgun.once('sent', function (message) {
+    if (test) {
+      test.equal(message.to, email, 'to address')
+      test.assert(message.subject.includes('Add'), 'subject has "Add"')
+      test.assert(
+        message.paragraphs.some(function (paragraph) {
+          return paragraph.includes(
+            'https://' + constants.HOSTNAME + '/add'
+          )
+        }),
+        'sent capability link'
+      )
+    }
+    callback(message)
+  })
+  http.request({path: '/add', method: 'POST', port})
+    .once('response', function (response) {
+      if (test) {
+        test.equal(
+          response.statusCode, 200,
+          'responds 200'
+        )
+        concat(response, function (error, buffer) {
+          var body = buffer.toString()
+          test.ifError(error, 'no error')
+          test.equal(
+            body, JSON.stringify({message: 'e-mail sent'}),
+            'e-mail sent message'
+          )
+        })
+      }
+    })
+    .end(JSON.stringify(add))
+}
--- test/add.test.js
+++ test/add.test.js
@@ -1,9 +1,6 @@
-var concat = require('simple-concat')
+var add = require('./add')
 var confirmSubscribe = require('./confirm-subscribe')
-var http = require('http')
-var makeKeyPair = require('./make-key-pair')
 var server = require('./server')
-var sign = require('./sign')
 var subscribe = require('./subscribe')
 var tape = require('tape')
 
@@ -12,37 +9,10 @@ tape('POST /add', function (test) {
     var email = 'test@example.com'
     subscribe(email, port, null, function (message) {
       confirmSubscribe(message, port, null, function () {
-        var keyPair = makeKeyPair()
-        var message = {
-          name: 'test device',
-          date: new Date().toISOString(),
-          email
-        }
-        var add = {
-          publicKey: keyPair.publicKey.toString('hex'),
-          signature: sign(message, keyPair.secretKey).toString('hex'),
-          message
-        }
-        http.request({path: '/add', method: 'POST', port})
-          .once('response', function (response) {
-            if (test) {
-              test.equal(
-                response.statusCode, 200,
-                'responds 200'
-              )
-              concat(response, function (error, buffer) {
-                var body = buffer.toString()
-                test.ifError(error, 'no error')
-                test.equal(
-                  body, JSON.stringify({message: 'e-mail sent'}),
-                  'e-mail sent message'
-                )
-              })
-              test.end()
-              done()
-            }
-          })
-          .end(JSON.stringify(add))
+        add(email, port, test, function () {
+          test.end()
+          done()
+        })
       })
     })
   })
