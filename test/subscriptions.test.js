4bb58c9d032c4894eb65370b952fd8ddc8ded5a2
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-28 10:51:23 -0700

    Test POST /cancel for nonexistent user

--- test/subscriptions.test.js
+++ test/subscriptions.test.js
@@ -1,3 +1,4 @@
+var FormData = require('form-data')
 var cancel = require('./cancel')
 var concat = require('simple-concat')
 var confirmCancel = require('./confirm-cancel')
@@ -159,6 +160,43 @@ tape('POST /cancel', function (test) {
         })
       })
     })
+  })
+})
+
+tape('POST /cancel', function (test) {
+  server(function (port, done) {
+    var email = 'notauser@example.com'
+    var form = new FormData()
+    form.append('email', email)
+    var request = http.request({
+      method: 'POST',
+      path: '/cancel',
+      headers: form.getHeaders(),
+      port
+    })
+      .once('response', function (response) {
+        if (test) {
+          test.equal(
+            response.statusCode, 200,
+            'responds 200'
+          )
+          concat(response, function (error, buffer) {
+            var body = buffer.toString()
+            test.ifError(error, 'no error')
+            var string = 'E-Mail Sent'
+            test.assert(
+              body.includes(string),
+              'body includes ' + JSON.stringify(string)
+            )
+            test.end()
+            done()
+          })
+        }
+      })
+      .once('error', function (error) {
+        test.ifError(error)
+      })
+    form.pipe(request)
   })
 })
 
