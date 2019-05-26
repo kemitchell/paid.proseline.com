a88795b87de1aff44eaab5730a0da77551bf45f5
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-26 14:17:33 -0700

    WIP

--- http.js
+++ http.js
@@ -1,11 +1,14 @@
 var AJV = require('ajv')
 var Busboy = require('busboy')
+var ReplicationProtocol = require('./protocol')
 var assert = require('assert')
+var async = require('async')
 var concatLimit = require('simple-concat-limit')
 var crypto = require('@proseline/crypto')
 var data = require('./data')
 var fs = require('fs')
 var keyserverProtocol = require('./keyserver-protocol')
+var lengthPrefixedStream = require('length-prefixed-stream')
 var parse = require('json-parse-errback')
 var pinoHTTP = require('pino-http')
 var runParallel = require('run-parallel')
@@ -69,6 +72,14 @@ module.exports = function (serverLog) {
     }
     if (pathname === '/invitation') {
       if (method === 'POST') return invitation(request, response)
+      return respond405(request, response)
+    }
+    var match = /^\/replicate\/([a-f0-9]{64})$/.exec(pathname)
+    if (match) {
+      var projectDiscoveryKey = match[1]
+      if (method === 'POST') {
+        return replicate(projectDiscoveryKeys, request, response)
+      }
       return respond405(request, response)
     }
     return notFound(request, response)
@@ -1017,6 +1028,176 @@ function ensureActiveSubscription (publicKey, callback) {
           callback(null, email, subscription)
         }
       )
+    })
+  })
+}
+
+function replicate (projectDiscoveryKey, request, response) {
+  var log = request.log
+  data.getProjectKeys(projectDiscoveryKey, function (error, keys) {
+    if (error) {
+      request.log.error(error)
+      response.statusCode = 500
+      return response.end()
+    }
+    var replicationKey = keys.replicationKey
+
+    // For each log, track the highest index that we believe our
+    // peer has, and use it to avoid sending unnecessary offers.
+    var heads = new Map()
+
+    function advancePeerHead (reference) {
+      var logPublicKey = reference.logPublicKey
+      var index = reference.index
+      var current = heads.get(logPublicKey)
+      if (current === undefined) return
+      if (index > current) heads.set(logPublicKey, index)
+    }
+
+    function shouldSend (reference) {
+      if (closed) return false
+      var current = heads.get(reference.logPublicKey)
+      if (current === undefined) return true
+      if (reference.index > current) return true
+      return false
+    }
+
+    var protocol = new ReplicationProtocol({
+      key: Buffer.from(replicationKey, 'hex')
+    })
+
+    var receivedData = false
+    setTimeout(function () {
+      if (!receivedData) close()
+    }, 1000)
+
+    protocol.once('handshake', function () {
+      log.info('received handshake')
+      protocol.handshake(function (error) {
+        if (error) return log.error(error)
+        log.info('sent handshake')
+      })
+      offerEnvelopes()
+    })
+
+    // 1. Sending
+
+    // When our peer requests an envelope...
+    var requestQueue = async.queue(sendEnvelope, 1)
+
+    protocol.on('request', function (request) {
+      receivedData = true
+      log.info(request, 'received request')
+      requestQueue.push(request, function (error) {
+        if (error) log.error(error)
+      })
+    })
+
+    function sendEnvelope (reference, done) {
+      var logPublicKey = reference.logPublicKey
+      var index = reference.index
+      data.getOuterEnvelope(
+        projectDiscoveryKey, logPublicKey, index,
+        function (error, outerEnvelope) {
+          if (error) return done(error)
+          if (!outerEnvelope) return done()
+          log.info(reference, 'sending envelope')
+          protocol.outerEnvelope(outerEnvelope, function (error) {
+            if (error) return done(error)
+            advancePeerHead(reference)
+            log.info(reference, 'sent envelope')
+            done()
+          })
+        }
+      )
+    }
+
+    function offerEnvelopes () {
+      data.listLogPublicKeys(projectDiscoveryKey, function (error, logPublicKeys) {
+        if (error) return log.error(error)
+        log.info({ logPublicKeys }, 'log public keys')
+        logPublicKeys.forEach(function (logPublicKey) {
+          data.getLastIndex(projectDiscoveryKey, logPublicKey, function (error, index) {
+            if (error) return log.error(error)
+            if (index === undefined) {
+              return log.error({ projectDiscoveryKey, logPublicKey }, 'no envelopes')
+            }
+            log.info({ logPublicKey, index }, 'last index')
+            sendOffer({ logPublicKey, index })
+          })
+        })
+      })
+    }
+
+    function sendOffer (reference) {
+      if (!shouldSend(reference)) return
+      log.info({
+        logPublicKey: reference.logPublicKey,
+        index: reference.index
+      }, 'sending offer')
+      protocol.offer(reference, function (error) {
+        if (error) return log.error(error)
+        log.info({}, reference, 'sent offer')
+      })
+    }
+
+    protocol.on('offer', function (reference) {
+      receivedData = true
+      log.info(reference, 'received offer')
+      advancePeerHead(reference)
+    })
+
+    protocol.on('outerEnvelope', function (outerEnvelope) {
+      receivedData = true
+      var logPublicKey = outerEnvelope.logPublicKey
+      var index = outerEnvelope.index
+      log.info({ logPublicKey, index }, 'received envelope')
+      advancePeerHead({ logPublicKey, index })
+      if (outerEnvelope.projectDiscoveryKey !== projectDiscoveryKey) {
+        return log.error({ logPublicKey, index }, 'project mismatch')
+      }
+      log.info({ logPublicKey, index }, 'putting outer envelope')
+      // TODO: Validate envelopes.
+      if (index === 0) {
+        data.putLogPublicKey(
+          projectDiscoveryKey, logPublicKey,
+          function (error) {
+            if (error) return log.error(error)
+            log.info({ projectDiscoveryKey, logPublicKey }, 'put public key')
+          }
+        )
+      }
+      data.putOuterEnvelope(outerEnvelope, function (error) {
+        if (error) return log.error(error)
+        log.info({ logPublicKey, index }, 'put envelope')
+        events.emit(`project:${projectDiscoveryKey}`, { logPublicKey, index })
+      })
+    })
+
+    protocol.on('invalid', function (message) {
+      log.error({ message }, 'invalid')
+    })
+
+    protocol.on('error', function (error) {
+      log.error(error)
+    })
+
+    protocol.once('close', close)
+    socket.once('close', close)
+    var closed = false
+    function close () {
+      if (closed) return
+      log.info('closing')
+      events.removeListener(eventName, onEnvelopeEvent)
+      protocol.end()
+      socket.end()
+      closed = true
+    }
+
+    request.pipe(protocol)
+    request.once('end', function () {
+      protocol.pipe(response)
+      offerEnvelopes()
     })
   })
 }
